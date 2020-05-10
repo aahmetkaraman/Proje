2c42c145e12a64c0d3b9f4f549ae541032ab5624
aahmetkaraman <61651573+aahmetkaraman@users.noreply.github.com>
2020-05-10 18:41:45 +0300

    Update login.php

--- web-sitesi-son/login.php
+++ web-sitesi-son/login.php
@@ -15,10 +15,10 @@ $sifre=123456;
 
 if($_POST["email"]==$eposta and $_POST["password"]==$sifre){
     echo "Hoşgeldiniz G191210023. Yönlendiriliyorsunuz";
-    Yonlendir("http://localhost/web-sitesi/index.html",3);
+    Yonlendir("http://localhost/web-sitesi-son/index.html",3);
 }
 else{
     echo "Hatalı Giriş Yaptınız. Yönlendiriliyorsunuz";
-    Yonlendir("http://localhost/web-sitesi/login.html",2);
+    Yonlendir("http://localhost/web-sitesi-son/login.html",2);
 }
 ?>
