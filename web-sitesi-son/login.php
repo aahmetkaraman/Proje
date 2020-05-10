3be0e85d0279dc0a6a9dd69ad80b03841a988176
aahmetkaraman <61651573+aahmetkaraman@users.noreply.github.com>
2020-05-10 18:37:01 +0300

    Bitti
    Her şey bitti

---
+++ web-sitesi-son/login.php
@@ -0,0 +1,24 @@
+<?php 
+$url="login.html";
+
+function Yonlendir($url,$zaman=0)
+{
+        if($zaman!=0){
+            header("Refresh:$zaman;url=$url");
+        }
+        else {
+            header("Location:$url");
+        }
+}
+$eposta= "g191210023@sakarya.edu.tr";
+$sifre=123456;
+
+if($_POST["email"]==$eposta and $_POST["password"]==$sifre){
+    echo "Hoşgeldiniz G191210023. Yönlendiriliyorsunuz";
+    Yonlendir("http://localhost/web-sitesi/index.html",3);
+}
+else{
+    echo "Hatalı Giriş Yaptınız. Yönlendiriliyorsunuz";
+    Yonlendir("http://localhost/web-sitesi/login.html",2);
+}
+?>
